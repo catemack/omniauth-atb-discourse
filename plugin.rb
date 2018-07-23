6a45d0b878da4a8f93937c377f5358cad0e8fc6a
Cate Mackenzie <catem@evidentpoint.com>
2018-07-23 11:14:13 -0700

    Adds first pass at atb omniauth strategy

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,33 @@
 # about: Plugin for Discourse to allow OAuth via ActiveTextbook
 # version: 0.0.1
 # authors: Catherine Mackenzie
+
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class ActiveTextbook < OmniAuth::Strategies::OAuth2
+      option :name, :atb
+      option :client_options, {
+        site: '',
+        authorize_url: '/oauth/authorize',
+        token_url: '/oauth/token'                        
+      }
+
+      uid { raw_info['id'] }
+
+      info do
+        {
+          email: raw_info['email'],
+          first_name: raw_info['first_name'],
+          last_name: raw_info['last_name'],
+          student: raw_info['student']
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/me').parsed
+      end
+    end
+  end
+end
